3dcebc6611e808dd967fb4ccd070611346b60915
Bart Joy <bj@bart.org.nz>
2015-01-17 07:07:37 +1300

    Completely redesigned most of the app. This is the first version that works with my hardware. The code is shocking.

--- Arduino/PsDesk/PsDesk.ino
+++ Arduino/PsDesk/PsDesk.ino
@@ -1,26 +1,25 @@
 #include <Wire.h>
 #include <EEPROM.h>
+#include <SPI.h>
 #include "MotorDriver.h"
 #include "Interval.h"
 
-#define LEFT_MOTOR_REVERSED false
+#define LEFT_MOTOR_REVERSED true
 #define RIGHT_MOTOR_REVERSED true
 
 #define LEFT_MOTOR_ENABLE_PIN_A 8
-#define LEFT_MOTOR_ENABLE_PIN_B 9
-#define LEFT_MOTOR_ISENSE_PIN_A 2
-#define LEFT_MOTOR_ISENSE_PIN_B 3
-#define LEFT_MOTOR_PWM_PIN_A 11
-#define LEFT_MOTOR_PWM_PIN_B 10
+#define LEFT_MOTOR_ENABLE_PIN_B 7
+#define LEFT_MOTOR_ISENSE_PIN_A 2  //analog
+#define LEFT_MOTOR_ISENSE_PIN_B 3  //analog
+#define LEFT_MOTOR_PWM_PIN_A 6     //PWM capable digital
+#define LEFT_MOTOR_PWM_PIN_B 9     //PWM capable digital
 
 #define RIGHT_MOTOR_ENABLE_PIN_A 4
-#define RIGHT_MOTOR_ENABLE_PIN_B 3
-#define RIGHT_MOTOR_ISENSE_PIN_A 0
-#define RIGHT_MOTOR_ISENSE_PIN_B 1
-#define RIGHT_MOTOR_PWN_PIN_A 5
-#define RIGHT_MOTOR_PWN_PIN_B 6
-
-#define ACCELEROMETER_SENSE_PIN 5
+#define RIGHT_MOTOR_ENABLE_PIN_B 2
+#define RIGHT_MOTOR_ISENSE_PIN_A 0  //analog
+#define RIGHT_MOTOR_ISENSE_PIN_B 1  //analog
+#define RIGHT_MOTOR_PWN_PIN_A 5     //PWM capable digital
+#define RIGHT_MOTOR_PWN_PIN_B 3     //PWM capable digital
 
 #define TIMER_DIVISOR 64
 #define OUT_OF_LEVEL_TRIPLEVEL 5
@@ -36,15 +35,66 @@ RIGHT_MOTOR_ISENSE_PIN_A, RIGHT_MOTOR_ISENSE_PIN_B,
 RIGHT_MOTOR_PWN_PIN_A, RIGHT_MOTOR_PWN_PIN_B, 
 RIGHT_MOTOR_REVERSED);
 
-double upSpeedFactor = 1, downSpeedFactor = 0.75;
-int moveDirection = 0;
-bool moving = false;
-double currentHeight = 0;
-double currentLevel = 0;
-double desiredLevel = readDesiredLevelFromEeprom();
+//ACCELEROMETER BEGIN
+#define MAX_INT 32767
+int accelerometerLevel = MAX_INT;
 
-unsigned long height = 0;
-unsigned long desiredHeight = 0;
+//Assign the Chip Select signal to pin 10.
+int CS=10;
+
+//This is a list of some of the registers available on the ADXL345.
+//To learn more about these and the rest of the registers on the ADXL345, read the datasheet!
+char POWER_CTL = 0x2D;	//Power Control Register
+char DATA_FORMAT = 0x31;
+char DATAX0 = 0x32;	//X-Axis Data 0
+char DATAX1 = 0x33;	//X-Axis Data 1
+char DATAY0 = 0x34;	//Y-Axis Data 0
+char DATAY1 = 0x35;	//Y-Axis Data 1
+char DATAZ0 = 0x36;	//Z-Axis Data 0
+char DATAZ1 = 0x37;	//Z-Axis Data 1
+
+//This function will write a value to a register on the ADXL345.
+//Parameters:
+//  char registerAddress - The register to write a value to
+//  char value - The value to be written to the specified register.
+void writeRegister(char registerAddress, char value){
+  //Set Chip Select pin low to signal the beginning of an SPI packet.
+  digitalWrite(CS, LOW);
+  //Transfer the register address over SPI.
+  SPI.transfer(registerAddress);
+  //Transfer the desired register value over SPI.
+  SPI.transfer(value);
+  //Set the Chip Select pin high to signal the end of an SPI packet.
+  digitalWrite(CS, HIGH);
+}
+
+//This function will read a certain number of registers starting from a specified address and store their values in a buffer.
+//Parameters:
+//  char registerAddress - The register addresse to start the read sequence from.
+//  int numBytes - The number of registers that should be read.
+//  char * values - A pointer to a buffer where the results of the operation should be stored.
+void readRegister(char registerAddress, int numBytes, char * values){
+  //Since we're performing a read operation, the most significant bit of the register address should be set.
+  char address = 0x80 | registerAddress;
+  //If we're doing a multi-byte read, bit 6 needs to be set as well.
+  if(numBytes > 1)address = address | 0x40;
+  
+  //Set the Chip select pin low to start an SPI packet.
+  digitalWrite(CS, LOW);
+  //Transfer the starting register address that needs to be read.
+  SPI.transfer(address);
+  //Continue to read registers until we've read the number specified, storing the results to the input buffer.
+  for(int i=0; i<numBytes; i++){
+    values[i] = SPI.transfer(0x00);
+  }
+  //Set the Chips Select pin high to end the SPI packet.
+  digitalWrite(CS, HIGH);
+}
+//ACCELEROMETER END
+
+double upSpeedFactor = 1, downSpeedFactor = 0.75;
+
+enum { MOVE_DIRECTION_UP, MOVE_DIRECTION_DOWN, MOVE_DIRECTION_NONE } moveDirection = MOVE_DIRECTION_NONE;
 
 void setup()
 {   
@@ -56,53 +106,27 @@ void setup()
   stopMove();
 
   Serial.begin(19200);
-  analogReference(EXTERNAL);
+  
+  //ACCEL
+    //Initiate an SPI communication instance.
+  SPI.begin();
+  //Configure the SPI connection for the ADXL345.
+  SPI.setDataMode(SPI_MODE3);
+  //Set up the Chip Select pin to be an output from the Arduino.
+  pinMode(CS, OUTPUT);
+  //Before communication starts, the Chip Select pin needs to be set high.
+  digitalWrite(CS, HIGH);
+  //Put the ADXL345 into +/- 4G range by writing the value 0x01 to the DATA_FORMAT register.
+  writeRegister(DATA_FORMAT, 0x01);
+  //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
+  writeRegister(POWER_CTL, 0x08);  //Measurement mode  
+  //ACCEL
 }
 
 void stopMove() {
   leftMotor.StopMove();
   rightMotor.StopMove();
-  moving = false;
-  moveDirection = 0;
-}
-
-void calcSpeed(int direction, double levelError, byte& leftSpeedResult, byte& rightSpeedResult) {
-  if (direction < 0) {
-    levelError *= -1;
-  }
-  
-  // Calculate speed drop based on level error
-  double absError = abs(levelError);
-  if (absError > 2) absError = 2;
-  double clampFactor = (absError / 2) * 255.0;
-  
-  // Assume going UP right now.
-  // Positive error means LEFT is too low so RIGHT must be clamped.
-  double leftSpeed = 255 - (levelError < 0 ? clampFactor : 0);
-  double rightSpeed = 255 - (levelError > 0 ? clampFactor : 0);  
-
-  double speedFactor = moveDirection > 0 ? upSpeedFactor : downSpeedFactor;
-  leftSpeedResult = (byte)(speedFactor * leftSpeed);
-  rightSpeedResult = (byte)(speedFactor * rightSpeed); 
-}
-
-void setMove() {
-  int direction = 0;
-  if (height < desiredHeight) {
-    direction = 1;
-  }
-  else if (height > desiredHeight) {
-    direction = -1;
-  } 
-  else {
-    return;
-  }
-
-  moveDirection = direction;
-  leftMotor.Move(direction, 127 * moveDirection);
-  rightMotor.Move(direction, 127 * moveDirection);
-  delay(200);
-  moving = true;
+  moveDirection = MOVE_DIRECTION_NONE;
 }
 
 bool writeLevelInfo = false;
@@ -117,52 +141,25 @@ void loop()
   while (Serial.available() > 0) {
     byte cmd = Serial.read();
     String s;
-    double newDesiredLevel;
 
     switch (cmd) {
     case '!': // STOP!
       stopMove();
-      Serial.println("OK: Stop");
+      Serial.println("Stopped.");
       break;
 
-    case 'M': // Move
-      desiredHeight = Serial.parseInt();
-      setMove();
-      s = "OK: MoveTo=";
-      s += desiredHeight;
+    case 'u': // Move up
+      Serial.println("Moving up...");
+      accelerometerLevel = MAX_INT;
+      leftMotor.Move(1, 255);
+      rightMotor.Move(1, 255);
       break;
 
-    case 'F': // Feed level data continuously
-      writeLevelInfo = true;
-      Serial.println("OK: LevelDataOutputOn");
-      break;
-
-    case 'G': // Stop feeding level data
-      writeLevelInfo = false;
-      Serial.println("OK: LevelDataOutputOff");
-      break;
-
-    case 'H': // Write desired level to EEPROM
-      newDesiredLevel = Serial.parseFloat();
-      desiredLevel = writeDesiredLevelToEeprom(newDesiredLevel);
-      s = String("OK: SetDesiredLevel=") + desiredLevel;
-      Serial.println(s);       
-      break;
-
-    case 'I': // Get desired level from EEPROM and output
-      desiredLevel = readDesiredLevelFromEeprom();
-      s = String("OK: DesiredLevel=") + desiredLevel + " DesiredHeight=" + desiredHeight;
-      Serial.println(s);       
-      break;
-
-    case 'J': // Feed height data continuously
-      writeHeightInfo = true;
-      Serial.println("OK: HeightDataOutputOn");
-      break;
-
-    case 'K': // Stop feeding height data
-      writeHeightInfo = false;
-      Serial.println("OK: HeightDataOutputOff");
+    case 'd': // Move down
+      Serial.println("Moving down...");
+      accelerometerLevel = MAX_INT;     
+      leftMotor.Move(-1, 255);
+      rightMotor.Move(-1, 255);
       break;
 
     default:
@@ -171,113 +168,22 @@ void loop()
     }
   }
 
-  heartbeat(writeLevelInfo);
+//ACCEL
+  char values[10];
+  //Reading 6 bytes of data starting at register DATAX0 will retrieve the x,y and z acceleration values from the ADXL345.
+  //The results of the read operation will get stored to the values[] buffer.
+  readRegister(DATAX0, 6, values);
+  //The ADXL345 gives 10-bit acceleration values, but they are stored as bytes (8-bits). To get the full value, two bytes must be combined for each axis.
+  //The X value is stored in values[0] and values[1].
+  const int x = ((int)values[1]<<8)|(int)values[0];
+//ACCEL
 
-  if (moving) {
-    double level = (desiredLevel - currentLevel);
+  if(accelerometerLevel == MAX_INT)
+    accelerometerLevel = x;  //calibrate
 
-    byte leftSpeed = 0, rightSpeed = 0;
-    calcSpeed(moveDirection, level, leftSpeed, rightSpeed);
-  
-    leftMotor.Move(moveDirection, leftSpeed);
-    rightMotor.Move(moveDirection, rightSpeed);
-  }
-
-  if (moving) {
-    if (moveDirection < 0 && height <= desiredHeight) {
-      stopMove();
-      Serial.println("MOVE-COMPLETE");
-
-    } 
-    else if (moveDirection > 0 && height >= desiredHeight) {
-      stopMove();
-      Serial.println("MOVE-COMPLETE");
-    }
-
-    if (abs(currentLevel - desiredLevel) > OUT_OF_LEVEL_TRIPLEVEL) {
-      if (outOfLevelCheckInterval.elapsed()) {
-        stopMove();
-        Serial.println("OUT_OF_LEVEL");
-      }
-    } 
-    else {
-      outOfLevelCheckInterval.reset();
-    }
+  if(abs(accelerometerLevel - x) > 20)
+  {
+   stopMove(); 
+   Serial.println("EMERGENCY STOP INITIATED BY ACCELEROMETER!");
   }
 }
-
-unsigned long accValues = 0;
-unsigned long accCount = 0;
-unsigned long heightValues = 0;
-unsigned long heightCount = 0;
-Interval accReadInterval(1, TIMER_DIVISOR);
-Interval accCalcInterval(150, TIMER_DIVISOR);
-Interval heartbeatInterval(1000, TIMER_DIVISOR);
-
-void heartbeat(bool writeLevelInfo) {
-  if (accReadInterval.elapsed()) {
-    int val = analogRead(ACCELEROMETER_SENSE_PIN);
-    accCount++;
-    accValues += val;
-
-    heightCount++;
-    heightValues += height;
-  }
-
-  if (accCalcInterval.elapsed()) {
-    currentLevel = (double)accValues / (double)accCount;
-    accCount = 0;
-    accValues = 0;
-    currentHeight = (double)heightValues / (double)heightCount;
-    heightValues = 0;
-    heightCount = 0;
-
-    if (writeLevelInfo) {
-      String s("L=");
-      s += currentLevel;
-      s += " D=";
-      s += desiredLevel;
-      s += " E=";
-      s += (desiredLevel - currentLevel);
-      Serial.println(s);
-    }
-
-    if (writeHeightInfo) {
-      String s("HEIGHT=");
-      s += currentHeight;
-      Serial.println(s);
-    }
-  }
-
-  if (heartbeatInterval.elapsed()) {
-    String s("PSDESK");
-    Serial.println(s);    
-  }
-}
-
-double readDesiredLevelFromEeprom() {
-  return EEPROM_readDouble(0);
-}
-
-double writeDesiredLevelToEeprom(double newValue) {
-  EEPROM_writeDouble(0, newValue);
-  return EEPROM_readDouble(0);
-}
-
-void EEPROM_writeDouble(int ee, double value)
-{
-  byte* p = (byte*)(void*)&value;
-  for (int i = 0; i < sizeof(value); i++)
-    EEPROM.write(ee++, *p++);
-}
-
-double EEPROM_readDouble(int ee)
-{
-  double value = 0.0;
-  byte* p = (byte*)(void*)&value;
-  for (int i = 0; i < sizeof(value); i++)
-    *p++ = EEPROM.read(ee++);
-  return value;
-}
-
-
